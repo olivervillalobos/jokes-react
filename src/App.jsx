aa95c16be4f0aeed17a5eb1eb095b8186421b887
Oliver Villalobos <71286508+olivervillalobos@users.noreply.github.com>
2025-06-14 12:32:32 -0700

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import Joke from "./Joke"
+import jokesData from "./jokeData"
 
 export default function App(){
-    const ninjaTurtles = ["Donatello","Raphael","Leonardo","Michaelangelo"]
-    const myMap = ninjaTurtles.map(ninja => {
-        const jsx = <h2>{ninja}</h2>
-        return jsx;
+    const jokeElements = jokesData.map((joke)=>{
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
     })
     return(
         <main>
-            {myMap}
+            {jokeElements}
         </main>
     )
 }
-
-/*<Joke 
-                setup="Why don't skeletons fight each other?"
-                punchline="They don't have the guts."
-            />
-            <Joke 
-                setup="What do you call fake spaghetti?"
-                punchline="An impasta."
-            />
-            <Joke 
-                setup="Why did the scarecrow win an award?"
-                punchline="Because he was outstanding in his field."
-            />
-            <Joke 
-                setup="Why can't your nose be 12 inches long?"
-                punchline="Because then it would be a foot."
-            />
-            <Joke 
-                punchline="I told my wife she was drawing her eyebrows too high—she looked surprised."
-            />
-            <Joke 
-                punchline="I used to play piano by ear, but now I use my hands."
-            /> */
